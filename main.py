b4e391d0388d3160978641719b1166d02742a9ec
trawwa <inekkit@gmail.com>
2022-07-01 08:09:31 +0700

    [upd] created main window

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-hi
+from tkinter import *
+
+class Paint(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+
+
+def main():
+    root = Tk()
+    root.geometry("1280x800+300+300")
+    app = Paint(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
